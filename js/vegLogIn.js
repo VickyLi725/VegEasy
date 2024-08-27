c14950813570f3c712fa765b1c75dfbf8138a807
VickyLi725 <x6918x@gmail.com>
2024-08-27 09:49:51 +0800

    暫時將登入後頁面跳轉回到index

--- js/vegLogIn.js
+++ js/vegLogIn.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     // 假設正確的帳號和密碼是 "user" 和 "password"
     if (account === 'abc' && password === '123') {
-        window.location.href = 'welcome.html'; // 登入成功後跳轉到歡迎頁面
+        window.location.href = 'index.html'; // 登入成功後跳轉到歡迎頁面
     } else {
         errorMessage.textContent = '帳號或密碼錯誤';
         errorMessage.style.visibility = 'visible'; // 顯示錯誤信息
